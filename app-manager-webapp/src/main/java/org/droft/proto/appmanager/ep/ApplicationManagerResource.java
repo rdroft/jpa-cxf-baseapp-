2326ea897fdbaf2f6281c4e3ddfcbd2d00f3a031
Roland Pashniev <roland@MacBook-Pro.local>
2016-01-23 15:40:25 +0000

    add jersey json support

---
+++ app-manager-webapp/src/main/java/org/droft/proto/appmanager/ep/ApplicationManagerResource.java
@@ -0,0 +1,11 @@
+package org.droft.proto.appmanager.ep;
+
+import javax.ws.rs.Path;
+
+/**
+ * Created by roland on 23/01/2016.
+ */
+
+@Path("appmanager")
+public class ApplicationManagerResource {
+}
---
+++ app-manager-webapp/src/main/java/org/droft/proto/appmanager/ep/VersionResource.java
@@ -0,0 +1,22 @@
+package org.droft.proto.appmanager.ep;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by roland on 23/01/2016.
+ */
+@Component
+@Path("version")
+public class VersionResource {
+
+    @GET
+    @Produces("application/json")
+    public String[] getApplicationVersion(){
+        return new String[]{"1.0.0","2,22"};
+    }
+}
